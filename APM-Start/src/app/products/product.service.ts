72be6bd206bb00bb5dcd4998dd3b359a7e205b52
Sarath Chandra NVG <snibhanupudi@micron.com>
2023-02-08 10:43:32 +0530

    rxjs reactive

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -14,13 +14,13 @@ export class ProductService {
   
   constructor(private http: HttpClient) { }
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
+  products$: Observable<Product[]> = 
+    this.http.get<Product[]>(this.productsUrl)
       .pipe(
         tap(data => console.log('Products: ', JSON.stringify(data))),
         catchError(this.handleError)
       );
-  }
+  
 
   private fakeProduct(): Product {
     return {
